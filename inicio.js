73d08d11b2c795478cce9e56cdd92a9287a88bb4
Gabriela V <gabrielav@unigranrio.br>
2022-06-19 16:01:57 -0300

    adicionando para teste

--- inicio.js
+++ inicio.js
@@ -3,3 +3,7 @@ let teste = "Estou testando meu git hub";
 let exemplo = "Ainda estou testando git hub";
 
 console.log("Vai ficar tudo bem");
+
+//alterando novamente para realizar um commit de teste
+
+let lista = ["maçã, banana, pera, uva"];

2c6b5d9e323d58b67ff9340b5c72ff98f6ab9f6a
Gabriela V <gabrielav@unigranrio.br>
2022-06-19 15:45:25 -0300

    adcionando soma.js

---
+++ inicio.js
@@ -0,0 +1,5 @@
+let teste = "Estou testando meu git hub";
+
+let exemplo = "Ainda estou testando git hub";
+
+console.log("Vai ficar tudo bem");
---
+++ soma.js
@@ -0,0 +1,6 @@
+let objeto = {
+    nome: cadeira,
+    cor: verde,
+    tamanho: pequena, 
+    idade: 89
+}
